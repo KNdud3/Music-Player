636719c6a6a75f7dfa24977ab4de14df0f0f8a57
Krishan Sachdev <krishansachdev@hotmail.co.uk>
2024-07-26 17:57:29 +0100

    preload and open file button (not functional)

--- my-app/src/render.js
+++ my-app/src/render.js
@@ -1,12 +1,27 @@
-const button = document.getElementById("test");
-const icon = document.getElementById("play");
+// Buttons
+const playButton = document.getElementById("pausePlay");
+const forwardButton = document.getElementById("forward");
+const backButton = document.getElementById("backward");
+const fileButton = document.getElementById("files")
 
-button.onclick = e => {
+
+const icon = document.getElementById("play"); // Inside of play button
+
+var audio = document.getElementById('music');
+
+playButton.onclick = e => {
   if (icon.classList.contains('fa-play')) {
     icon.classList.remove('fa-play');
+    audio.play().catch(function(error) {
+      console.log('Error playing audio:', error);
+    });
     icon.classList.add('fa-pause');
-  } else {
+  } 
+  else {
     icon.classList.remove('fa-pause');
+    audio.pause();
     icon.classList.add('fa-play');
   }
 };
+
+
